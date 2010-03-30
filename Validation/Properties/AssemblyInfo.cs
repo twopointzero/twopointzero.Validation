24e7896e2f621b6252e8c7959cd02dfcedc2e14c
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-30 15:09:33 -0700

    Added AssemblyInfoCommon.cs as per usual and imported the most recent set of R# settings.

---
+++ Validation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("twopointzero.Validation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("twopointzero.Validation")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("f9ee862c-65f4-446b-bdb5-4ef7f32a2982")]
