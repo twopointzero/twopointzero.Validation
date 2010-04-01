2c1daac4ed3a9000fd36f084ea8db833ad4325bc
Jeremy Gray <jeremy@jeremygray.ca>
2010-04-01 16:45:24 -0700

    Added static helper methods for those instance extension methods that had not yet been given them. All now do, and as more are added they will be given static helpers at the same time.

--- Validation/Validator.cs
+++ Validation/Validator.cs
@@ -1,4 +1,6 @@
-﻿namespace twopointzero.Validation
+﻿using System.Collections.Generic;
+
+namespace twopointzero.Validation
 {
     public static class Validator
     {
@@ -20,6 +22,17 @@
                                                            message).Validate();
         }
 
+        public static void IsEqualTo<T>(T actualValue, string paramName, T expectedValue) where T : class
+        {
+            ValidatorInstanceExtensions.IsEqualTo(null, actualValue, paramName, expectedValue, null).Validate();
+        }
+
+        public static void IsEqualTo<T>(T actualValue, string paramName, T expectedValue, string message)
+            where T : class
+        {
+            ValidatorInstanceExtensions.IsEqualTo(null, actualValue, paramName, expectedValue, message).Validate();
+        }
+
         public static void IsLessThan(byte actualValue, string paramName, byte expectedValue)
         {
             ValidatorInstanceExtensions.IsLessThan(null, actualValue, paramName, expectedValue, null).Validate();
@@ -38,6 +51,26 @@
         public static void IsNotNull<T>(T actualValue, string paramName, string message) where T : class
         {
             ValidatorInstanceExtensions.IsNotNull(null, actualValue, paramName, message).Validate();
+        }
+
+        public static void IsNotNullOrEmpty<T>(IEnumerable<T> actualValue, string paramName)
+        {
+            ValidatorInstanceExtensions.IsNotNullOrEmpty(null, actualValue, paramName, null).Validate();
+        }
+
+        public static void IsNotNullOrEmpty<T>(IEnumerable<T> actualValue, string paramName, string message)
+        {
+            ValidatorInstanceExtensions.IsNotNullOrEmpty(null, actualValue, paramName, message).Validate();
+        }
+
+        public static void IsNotNullOrEmpty(string actualValue, string paramName)
+        {
+            ValidatorInstanceExtensions.IsNotNullOrEmpty(null, actualValue, paramName, null).Validate();
+        }
+
+        public static void IsNotNullOrEmpty(string actualValue, string paramName, string message)
+        {
+            ValidatorInstanceExtensions.IsNotNullOrEmpty(null, actualValue, paramName, message).Validate();
         }
     }
 }
